bdc0863f5dde09144f4f80d22011eeba2ef0e02a
BearerOP <ankity1892003@gmail.com>
2025-01-04 18:58:42 +0530

    renamed loadscript 3

--- src/utils/loadScript.ts
+++ src/utils/loadScript.ts
@@ -1,14 +1,17 @@
 export function loadscript(src: string): Promise<boolean> {
-    return new Promise((resolve) => {
-      const script = document.createElement("script")
-      script.src = src
-      script.onload = () => {
-        resolve(true)
-      }
-      script.onerror = () => {
-        resolve(false)
-      }
-      document.body.appendChild(script)
-    })
-  }
+  return new Promise((resolve, reject) => {
+    if (document.querySelector(`script[src="${src}"]`)) {
+      resolve(true);
+      return;
+    }
 
+    const script = document.createElement("script");
+    script.src = src;
+    script.async = true;
+
+    script.onload = () => resolve(true);
+    script.onerror = () => reject(false);
+
+    document.body.appendChild(script);
+  });
+}
